1a1a8f06813bb85410203c47f76b567763d4ac8b
Robin Panta <hacktivistic@gmail.com>
2022-02-01 19:54:09 +0530

    feat: add type defn for helpers

--- types/modules.ts
+++ types/modules.ts
@@ -1,3 +1,4 @@
+declare let use: Function
 declare let app: Function
 declare let env: Function
 declare let config: Function
@@ -5,3 +6,6 @@ declare let base64_encode: Function
 declare let base64_decode: Function
 
 declare type KeyValue = { [key: string] : string }
+
+declare let ucfirst: Function
+declare let dd: Function
