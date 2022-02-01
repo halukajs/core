eea1bb5adc3a2b5d8d2d25c4d8afc7c9087d7ef5
Robin Panta <hacktivistic@gmail.com>
2022-02-01 19:54:22 +0530

    fix: change helpers from ts to js

--- src/Helpers/others.js
+++ src/Helpers/others.js
@@ -2,14 +2,14 @@
 
 /* istanbul ignore next */
 global.ucfirst = 
-function (str: string): string {
+function (str) {
 	return (str.charAt(0).toUpperCase() + str.slice(1))
 }
 
 // dd (Dump & Die)
 /* istanbul ignore next */
 global.dd = 
-function (...args: any): void {
+function (...args) {
 	console.log(...args)
 	process.exit(1)
 }
